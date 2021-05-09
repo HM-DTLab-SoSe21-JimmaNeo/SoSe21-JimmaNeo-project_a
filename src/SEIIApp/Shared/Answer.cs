bfa6e3e7cb80b21d2396c085116af7655484f5c8
Valentin Passon <passon@hm.edu>
2021-05-09 20:27:33 +0200

    erster commit

---
+++ src/SEIIApp/Shared/Answer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEIIApp.Shared
+{
+    public class Answer
+    {
+        [Required]
+        public string Text { get; set; }
+
+        public bool Selected { get; set; }
+    }
+}
---
+++ src/SEIIApp/Shared/SingleChoiceQuestion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SEIIApp.Shared
+{
+    public class SingleChoiceQuestion
+    {
+        [Required]
+        public string Question { get; set; }
+
+        [Required]
+        public List<Answer> Options { get; set; }
+
+        [Required]
+        public int Solution { get; set; }
+    }
+}
